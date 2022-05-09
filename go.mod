d723dc5e79a465fede5419e3f5279d7e4c245456
Kycer <978262587@qq.com>
2022-05-09 11:31:30 +0800

    init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module aliddns
+
+go 1.18
+
+require (
+	github.com/BurntSushi/toml v1.0.0
+	github.com/ahmetb/go-linq/v3 v3.2.0
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1528
+	github.com/jasonlvhit/gocron v0.0.1
+)
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/BurntSushi/toml"
+	"github.com/ahmetb/go-linq/v3"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
+	"github.com/jasonlvhit/gocron"
+)
+
+var CONFIG_PATH = []string{"~/.config/aliddns", "./configs"}
+
+var CONFIG_MAP = make(map[string]*DDNSConfig)
+
+//  主配置文件
+type DDNSConfig struct {
+	AliAccess *AliAccess
+	Domains   *[]Domain
+}
+
+type AliAccess struct {
+	AccessId  string
+	AccessKey string
+	Region    string
+	Domain    string
+}
+
+type Domain struct {
+	Rr         string
+	DomainType string
+	UpdateType string
+	Value      string
+}
+
+func getIP() string {
+	responseClient, errClient := http.Get("https://ipv4.ipw.cn/api/ip/myip")
+
+	if errClient != nil {
+		log.Printf("获取外网 IP 失败，请检查网络\n")
+		panic(errClient)
+	}
+	defer responseClient.Body.Close()
+
+	// 获取 http response 的 body
+	body, _ := ioutil.ReadAll(responseClient.Body)
+	clientIP := string(body)
+	return clientIP
+}
+
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+func loadConfig(path string) *DDNSConfig {
+	var ddns DDNSConfig
+	//加载toml文件
+	if _, err := toml.DecodeFile(path, &ddns); err != nil {
+		log.Println("decode config file err")
+	}
+	return &ddns
+}
+
+func loadConfigs() {
+	for _, v := range CONFIG_PATH {
+		files, _ := ioutil.ReadDir(v)
+		for _, f := range files {
+			filename := f.Name()
+			filesuffix := path.Ext(filename)
+			if f.IsDir() || filesuffix != ".toml" {
+				continue
+			}
+
+			configpath := path.Join(v, f.Name())
+			log.Printf("加载配置文件：%s", filename)
+			CONFIG_MAP[filename[0:len(filename)-len(filesuffix)]] = loadConfig(configpath)
+		}
+	}
+}
+
+func getClient(aliAccess *AliAccess) *alidns.Client {
+	client, _ := alidns.NewClientWithAccessKey(aliAccess.Region, aliAccess.AccessId, aliAccess.AccessKey)
+	return client
+}
+
+func getRecord(rr string, ddnsConfig *DDNSConfig, client *alidns.Client) alidns.Record {
+
+	request := alidns.CreateDescribeDomainRecordsRequest()
+	request.Scheme = "https"
+	request.DomainName = ddnsConfig.AliAccess.Domain
+	response, err := client.DescribeDomainRecords(request)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	// 过滤符合条件的子域名信息。
+	result := linq.From(response.DomainRecords.Record).Where(func(c interface{}) bool {
+		return c.(alidns.Record).RR == rr
+	}).First()
+
+	return result.(alidns.Record)
+
+}
+
+func updateDomain(ddnsConfig *DDNSConfig, domain *Domain, client *alidns.Client) {
+	record := getRecord(domain.Rr, ddnsConfig, client)
+	ip := domain.Value
+	if domain.UpdateType == "network" {
+		ip = getIP()
+	}
+	if ip == record.Value {
+		log.Printf("[%s]IP一致，无需更新 => [%s]", domain.Rr, ip)
+		return
+	}
+	request := alidns.CreateUpdateDomainRecordRequest()
+	request.Scheme = "https"
+	request.RecordId = record.RecordId
+	request.RR = domain.Rr
+	request.Type = domain.DomainType
+	request.Value = ip
+	log.Printf("更新[%s]: %s => %s", domain.Rr, record.Value, ip)
+	_, err := client.UpdateDomainRecord(request)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func update() {
+	for _, ddnsConfig := range CONFIG_MAP {
+		// 获取DDNS客户端
+		client := getClient(ddnsConfig.AliAccess)
+
+		for _, domain := range *ddnsConfig.Domains {
+			log.Println(domain)
+			updateDomain(ddnsConfig, &domain, client)
+		}
+	}
+}
+
+var run = flag.Bool("r", false, "bool类型参数")
+var one = flag.Bool("o", false, "string类型参数")
+
+func main() {
+	loadConfigs()
+	flag.Parse()
+	if *one {
+		log.Println("执行一次.............")
+		update()
+		log.Println("结束执行.............")
+	}
+
+	if *run {
+		log.Println("启动任务.............")
+		s := gocron.NewScheduler()
+		s.Every(30).Minutes().Do(update)
+		<-s.Start()
+	}
+
+}
